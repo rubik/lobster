a7d55174bae190ece8c9c9b3423593c9862836b9
Michele Lacchia <michelelacchia@gmail.com>
2020-09-23 14:55:48 +0200

    chore: add basic benchmarks

---
+++ benches/simple.rs
@@ -0,0 +1,37 @@
+use criterion::{criterion_main, criterion_group, Criterion};
+use lobster::{OrderBook, OrderType, Side};
+
+fn small_limit_ladder(c: &mut Criterion) {
+    c.bench_function("small limit ladder", |b| {
+        let mut ob = OrderBook::default();
+        b.iter(|| {
+            for i in 0..5_000 {
+                ob.event(OrderType::Limit {
+                    id: i as u128,
+                    price: 12345 + i as u64,
+                    qty: i as u64,
+                    side: Side::Bid,
+                });
+            }
+        });
+    });
+}
+
+fn big_limit_ladder(c: &mut Criterion) {
+    c.bench_function("big limit ladder", |b| {
+        let mut ob = OrderBook::default();
+        b.iter(|| {
+            for i in 0..100_000 {
+                ob.event(OrderType::Limit {
+                    id: i as u128,
+                    price: 12345 + i as u64,
+                    qty: i as u64,
+                    side: Side::Bid,
+                });
+            }
+        });
+    });
+}
+
+criterion_group!(benches, small_limit_ladder, big_limit_ladder);
+criterion_main!(benches);
