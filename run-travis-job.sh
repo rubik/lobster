726a350d2fac5cfb8e9f897aa7540f7c41ae8eed
Michele Lacchia <michelelacchia@gmail.com>
2020-10-11 10:19:41 +0200

    chore: update Travis script to run with all threads

--- run-travis-job.sh
+++ run-travis-job.sh
@@ -7,6 +7,6 @@ make $COMMAND
 if [ "$COV" = "yes" ]
 then
     cargo install cargo-tarpaulin
-    cargo tarpaulin -v --ignore-tests \
-        --ciserver travis-ci --coveralls "$TRAVIS_JOB_ID" -- --test-threads 1
+    cargo tarpaulin -v --ignore-tests --lib --ciserver travis-ci \
+        --coveralls "$TRAVIS_JOB_ID"
 fi
